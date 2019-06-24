6ef50ad14749019f8b88b0d23cf1aca5b3df2445
applebuddy <applebuddy@nate.com>
2019-06-24 22:58:37 +0900

    review BFS problem

--- BackJoonPractice/4_1_Graph_미로탐색_2178.cpp
+++ BackJoonPractice/4_1_Graph_미로탐색_2178.cpp
@@ -14,55 +14,43 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#define endl "\n"
-#define MAX 101
 using namespace std;
+typedef pair<int,int> Pair;
+int tx[] = {0,0,1,-1};
+int ty[] = {1,-1,0,0};
+vector<vector<int>> g(101,vector<int>(101,0));
+vector<vector<int>> c(101,vector<int>(101,0));
 
-int g[MAX][MAX];
-int c[MAX][MAX];
-int tx[] = {0,0,-1,1};
-int ty[] = {-1,1,0,0};
-int N,M;
-
-void BFS(int n, int m){
-    queue<pair<int,int>> q;
-    ios_base :: sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    q.push(make_pair(1,1));
-    c[1][1]=1;
+void checkMiro(int x, int y) {
+    queue<Pair> q;
+    q.push(make_pair(0, 0));
+    c[0][0]=1;
     
     while(!q.empty()){
-        int nx = q.front().first;
-        int ny = q.front().second;
+        int nx=q.front().first;
+        int ny=q.front().second;
         q.pop();
         for(int i=0; i<4; i++){
-            int rx = nx+tx[i];
-            int ry = ny+ty[i];
-            if(rx>=0 && rx<=N && ry>=0 && ry<=M){
-                if(c[rx][ry]==0 && g[rx][ry]==1){
-                    c[rx][ry]=c[nx][ny]+1;
-                    q.push(make_pair(rx,ry));
-                }
-            }
+            int rx=nx+tx[i];
+            int ry=ny+ty[i];
+            if(rx<0 || rx>x || ry<0 || ry>y) continue;
+            if(c[rx][ry]!=0 || g[rx][ry]==0) continue;
+            c[rx][ry]=c[nx][ny]+1;
+            q.push(make_pair(rx,ry));
         }
     }
 }
 
-int main(){
-
-    for(int i=0; i<MAX; i++){
-    memset(c[i],0,sizeof(c[i]));
-    }
+int main() {
     
+    int N,M;
     scanf("%d %d",&N,&M);
-    for(int i=1; i<=N; i++){
-        for(int j=1; j<=M; j++){
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
             scanf("%1d",&g[i][j]);
         }
     }
-    
-    BFS(N,M);
-    printf("%d\n",c[N][M]);
+    checkMiro(N, M);
+    printf("%d\n",c[N-1][M-1]);
     return 0;
 }
