7fdda80b93fa3642ca6b74e3842704446de39022
MinKyeongTae <kyeongtea92@gmail.com>
2019-11-27 23:39:08 +0900

    Solving 1 Basic Problem with Swift

---
+++ BackJoonPractice/Basic/설탕배달_2839.swift
@@ -0,0 +1,22 @@
+//
+//  설탕배달_2839.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2019/11/27.
+//  Copyright © 2019 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 설탕배달_2839
+
+/*
+ import Foundation
+
+ var num = Int(readLine()!)!
+
+ for i in 0...num/3 {
+ for j in 0...num/5 {
+     if(i*3 + j*5 == num) { print(i+j); exit(0) }
+ }
+ }
+ print(-1)
+ */
