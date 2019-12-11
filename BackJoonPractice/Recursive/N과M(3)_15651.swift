7cf90ad2753facf7d241388d210f422363ed0087
MinKyeongTae <kyeongtea92@gmail.com>
2019-12-11 17:42:56 +0900

    N과M(3)_15651 Swift 문제풀이

---
+++ BackJoonPractice/Recursive/N과M(3)_15651.swift
@@ -0,0 +1,32 @@
+//
+//  N과M(3)_15651.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2019/12/11.
+//  Copyright © 2019 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - N과M(3)_15651
+
+/*
+import Foundation
+
+var input = readLine()?.split(separator: " ").map { Int($0)! }
+let N = input![0], M = input![1]
+
+func DFS(_ idx: Int) {
+    if idx == M {
+        print(arr.joined(separator: " "))
+        return
+    }
+    
+    for i in 1...N {
+        arr[idx] = String(i)
+        DFS(idx+1)
+    }
+}
+
+var arr = [String](repeating: "1", count: M)
+DFS(0)
+
+*/
