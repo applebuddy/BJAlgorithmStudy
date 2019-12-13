6b37c0febae5fc351fbf8943562d1ac998e17019
MinKyeongTae <kyeongtea92@gmail.com>
2019-12-13 23:08:31 +0900

    사파리월드_2420 C++ 문제풀이

---
+++ BackJoonPractice/Basic/사파리월드_2420.cpp
@@ -0,0 +1,23 @@
+//
+//  사파리월드_2420.cpp
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2019/12/13.
+//  Copyright © 2019 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: 사파리월드_2420
+
+#if 0
+#include <iostream>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios_base :: sync_with_stdio(0); cin.tie(0);
+    ll a,b; cin>>a>>b;
+    printf("%lld\n",a>b ? a-b : b-a);
+    return 0;
+}
+#endif
