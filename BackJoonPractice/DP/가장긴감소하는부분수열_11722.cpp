93ff0f1d80f8f66066ea715768c5c87cb7509c6f
MinKyeongTae <kyeongtea92@gmail.com>
2019-12-13 15:18:12 +0900

    가장긴감소하는부분수열_11722 LIS C++ 문제풀이

--- BackJoonPractice/DP/가장긴감소하는부분수열_11722.cpp
+++ BackJoonPractice/DP/가장긴감소하는부분수열_11722.cpp
@@ -2,34 +2,33 @@
 //  가장긴감소하는부분수열_11722.cpp
 //  BackJoonPractice
 //
-//  Created by MinKyeongTae on 22/10/2019.
+//  Created by MinKyeongTae on 2019/12/13.
 //  Copyright © 2019 Min Kyeong Tae. All rights reserved.
 //
 
-/// MARK: - 가장긴감소하는부분수열_11722
+// MARK: - 가장 긴 감소하는 부분수열_11722
+
 #if 0
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-void theLongestDecreasingSubSequence() {
-    int T; cin>>T;
-    vector<int> V(T,0);
-    for(int i=0; i<T; i++) cin>>V[i];
-    reverse(V.begin(), V.end());
-    vector<int> Ans;
-    for(int i=0; i<V.size(); i++) {
-        int IDX = (int)(lower_bound(Ans.begin(), Ans.end(), V[i])-Ans.begin());
-        if(IDX==Ans.size()) Ans.push_back(V[i]);
-        else Ans[IDX]=V[i];
-    }
-    printf("%d\n",(int)Ans.size());
-    return;
-}
-
 int main() {
-    theLongestDecreasingSubSequence();
+    ios_base :: sync_with_stdio(0); cin.tie(0);
+    int N; cin>>N;
+    vector<int> V(N,0);
+    vector<int> Ans;
+    vector<int>::iterator iter = Ans.begin();
+    for(int i=0; i<N; i++) cin>>V[i];
+    reverse(V.begin(), V.end());
+    for(int i=0; i<N; i++) {
+        iter = lower_bound(Ans.begin(), Ans.end(), V[i]);
+        if(iter == Ans.end()) Ans.push_back(V[i]);
+        else Ans[iter-Ans.begin()] = V[i];
+    }
+    
+    printf("%d\n",(int)Ans.size());
     return 0;
 }
 #endif
