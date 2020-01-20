c115f40f199c46a2e711ae50d0fbfa347706698f
MinKyeongTae <kyeongtea92@gmail.com>
2020-01-21 00:20:17 +0900

    약수판별알고리즘 C++ 문제풀이

---
+++ BaekJoon/Basic/약수판별.cpp
@@ -0,0 +1,21 @@
+//
+//  약수판별.cpp
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/01/21.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base :: sync_with_stdio(0); cin.tie(0);
+    while(1) {
+    int A,B; cin>>A>>B;
+    if(A==0) break;
+    if(!(B%A)) puts("A는 B의 약수에요.");
+    else puts("A는 B의 약수가 아니에요.");
+    }
+    return 0;
+}
