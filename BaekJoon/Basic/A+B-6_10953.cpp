f4029a30b1a20f6c16b2aa710380cbc75e26733e
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-02 05:01:26 +0900

    A+B-6_10953 C++ 문제풀이

---
+++ BaekJoon/Basic/A+B-6_10953.cpp
@@ -0,0 +1,25 @@
+//
+//  A+B-6_10953.cpp
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/02/02.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: A+B-6
+// MARK: - 문제풀이
+#if 0
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    ios_base :: sync_with_stdio(0); cin.tie(0);
+    int T; cin>>T;
+    while(T--) {
+        string S; cin>>S;
+        printf("%d\n",S[0]-'0' + S[2]-'0');
+    }
+    return 0;
+}
+#endif
