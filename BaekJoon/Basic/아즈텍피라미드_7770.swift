f99190b3d89d7063fe8a23d067b8a0c1b3159845
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-07 19:40:35 +0900

    아즈텍피라미드_7770 Swift 문제풀이

---
+++ BaekJoon/Basic/아즈텍피라미드_7770.swift
@@ -0,0 +1,26 @@
+//
+//  아즈텍피라미드_7770.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/02/07.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: 아즈텍 피라미드 7770
+
+/*
+import Foundation
+
+let N = Int(readLine()!)!
+var sum = 0, Ans = 0, idx = 1
+if N == 1 { print(1); exit(0) }
+while sum <= N {
+    sum += idx * idx + (idx-1) * (idx-1)
+    if sum > N { break }
+    Ans += 1
+    idx += 1
+    
+}
+
+print(Ans)
+*/
