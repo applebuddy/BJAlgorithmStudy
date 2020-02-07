d6009d4ef58b751f34d7746786e75e25d37b05d0
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-07 19:03:50 +0900

    회사에있는사람_7785 Swift 문제풀이

---
+++ BaekJoon/Basic/회사에있는사람_7785.swift
@@ -0,0 +1,29 @@
+//
+//  회사에있는사람_7785.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/02/07.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: 회사에있는사람 7785
+// MARK: - 문제풀이
+/*
+import Foundation
+
+let N = Int(readLine()!)!
+var ST = Set<String>()
+for _ in 0..<N {
+    let arr = readLine()!.split(separator: " ").map { String($0) }
+    if arr[1] == "enter" {
+        ST.insert(arr[0])
+    } else {
+        ST.remove(arr[0])
+    }
+}
+
+let Ans = ST.sorted(by: >)
+for i in Ans.indices {
+    print(Ans[i])
+}
+*/
