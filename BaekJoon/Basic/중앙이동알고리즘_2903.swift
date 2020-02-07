8186cf515c875be3be7c89bb5e73c8a3c950f400
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-07 22:18:28 +0900

    중앙이동알고리즘_2903 swift 문제풀이

---
+++ BaekJoon/Basic/중앙이동알고리즘_2903.swift
@@ -0,0 +1,22 @@
+//
+//  중앙이동알고리즘_2903.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/02/07.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: 중앙이동알고리즘 2903
+// MARK: - 문제풀이
+/*
+import Foundation
+
+let N = Int(readLine()!)!
+var K = 3, Ans = 0
+for _ in 1...N {
+    Ans = K * K
+    K += K-1
+}
+
+print(Ans)
+*/
