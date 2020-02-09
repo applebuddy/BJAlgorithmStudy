58c90eee86627ca0d67b113446790532963d3ea1
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-09 20:21:40 +0900

    달팽이는올라가고싶다_2869 Math C++ 문제풀이

---
+++ BaekJoon/Math/달팽이는올라가고싶다_2869.cpp
@@ -0,0 +1,9 @@
+//
+//  달팽이는올라가고싶다_2869.cpp
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/02/09.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+#include <stdio.h>
