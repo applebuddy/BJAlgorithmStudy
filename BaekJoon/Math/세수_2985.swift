0c0fdf3f030c777248948e50dafd288c34d666b9
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-09 17:54:43 +0900

    세수_2985 Math swift 문제풀이

---
+++ BaekJoon/Math/세수_2985.swift
@@ -0,0 +1,31 @@
+//
+//  세수_2985.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/02/09.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: 세 수 2985
+// MARK: - 문제풀이
+/*
+import Foundation
+let arr = readLine()!.split(separator: " ").map { Int($0)! }
+
+func checkNum(_ A: Int, _ B: Int, _ C: Int) -> String? {
+    if A + B == C { return "+" }
+    else if A - B == C { return "-" }
+    else if A * B == C { return "*" }
+    else if A / B == C { return "/" }
+    else { return nil }
+}
+
+if let chk = checkNum(arr[0], arr[1], arr[2])
+{
+    print("\(arr[0])\(chk)\(arr[1])=\(arr[2])")
+}
+else if let chk2 = checkNum(arr[1], arr[2], arr[0])
+{
+    print("\(arr[0])=\(arr[1])\(chk2)\(arr[2])")
+}
+*/
