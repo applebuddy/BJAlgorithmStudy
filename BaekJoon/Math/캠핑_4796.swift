46a43d27ad8516d33a0032cdc73ae70646225ae2
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-09 17:25:04 +0900

    캠핑_4796 Math swift 문제풀이

---
+++ BaekJoon/Math/캠핑_4796.swift
@@ -0,0 +1,24 @@
+//
+//  캠핑_4796.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/02/09.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: 캠핑 4796
+// MARK: - 문제풀이
+/*
+import Foundation
+
+var idx = 1
+while true {
+    let arr = readLine()!.split(separator: " ").map { Int(String($0))! }
+    let L = arr[0]
+    let P = arr[1]
+    let V = arr[2]
+    if L==0 && P==0 && V==0 { break }
+    print("Case \(idx): \(V/P * L + min(L,V%P))")
+    idx += 1
+}
+*/
---
+++ BaekJoon/String/팰린드롬만들기_1213.swift
@@ -0,0 +1,9 @@
+//
+//  팰린드롬만들기_1213.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/02/09.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+import Foundation
