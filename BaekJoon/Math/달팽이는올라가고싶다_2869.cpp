4f0152f435d6d9dc5053be59821f9f8c5344db6d
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-10 17:34:15 +0900

    달팽이는올라가고싶다_2869 Math swift 문제풀이

--- BaekJoon/Math/달팽이는올라가고싶다_2869.cpp
+++ BaekJoon/Math/달팽이는올라가고싶다_2869.cpp
@@ -6,4 +6,25 @@
 //  Copyright © 2020 Min Kyeong Tae. All rights reserved.
 //
 
-#include <stdio.h>
+// MARK: 달팽이는 올라가고 싶다 2869
+// MARK: - 문제풀이
+
+// ex)
+//. 5 -> 4
+//. 9 -> 8
+//. 13 -> 12
+//. 17
+
+#if 0
+#include <iostream>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    ll A,B,V; cin>>A>>B>>V;
+    if(V <= A) return !printf("%d\n",1);
+    printf("%lld\n",(((V-A)-1)/(A-B)+1)+1);
+    return 0;
+}
+#endif
+
---
+++ BaekJoon/Math/달팽이는올라가고싶다_2869.swift
@@ -0,0 +1,18 @@
+//
+//  달팽이는올라가고싶다_2869.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/02/10.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: 달팽이는올라가고싶다 2869
+// MARK: - 문제풀이
+/*
+import Foundation
+
+let arr = readLine()!.split(separator: " ").map { Int($0)! }
+let A = arr[0], B = arr[1], V = arr[2]
+if A >= V { print(1); exit(0) }
+print((V-A-1)/(A-B)+2)
+*/
