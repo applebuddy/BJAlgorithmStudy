9e4fcb9243c3a7ce0b1cf41d193ac8ca6cba8bf5
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-13 19:11:29 +0900

    콜센터_5339 IO swift 문제풀이

---
+++ BaekJoon/IO/콜센터_5339.swift
@@ -0,0 +1,24 @@
+//
+//  콜센터_5339.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/02/13.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 콜센터 5339
+// MARK: 문제풀이
+/*
+import Foundation
+
+print("     /~\\")
+print("    ( oo|")
+print("    _\\=/_")
+print("   /  _  \\")
+print("  //|/.\\|\\\\ ")
+print(" ||  \\ /  ||")
+print("============")
+print("|          |")
+print("|          |")
+print("|          |")
+*/
