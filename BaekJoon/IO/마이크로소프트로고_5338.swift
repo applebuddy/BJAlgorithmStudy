99d5fced90af118f4cc4706258716482c69f448e
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-13 19:06:52 +0900

    마이크로소프트로고_5338 IO swift 문제풀이

---
+++ BaekJoon/IO/마이크로소프트로고_5338.swift
@@ -0,0 +1,19 @@
+//
+//  마이크로소프트로고_5338.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/02/13.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 마이크로소프트로고 5338
+// MARK: 문제풀이
+/*
+import Foundation
+
+print("       _.-;;-._")
+print("'-..-'|   ||   |")
+print("'-..-'|_.-;;-._|")
+print("'-..-'|   ||   |")
+print("'-..-'|_.-''-._|")
+*/
--- BaekJoon/IO/웰컴_5337.swift
+++ BaekJoon/IO/웰컴_5337.swift
@@ -6,4 +6,13 @@
 //  Copyright © 2020 Min Kyeong Tae. All rights reserved.
 //
 
+// MARK: - 웰컴 5337
+// MARK: 문제풀이
+/*
 import Foundation
+
+print(".  .   .")
+print("|  | _ | _. _ ._ _  _")
+print("|/\\|(/.|(_.(_)[ | )(/.")
+*/
+
