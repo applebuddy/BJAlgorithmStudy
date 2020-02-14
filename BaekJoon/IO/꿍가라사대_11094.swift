cae525d75484c63331ebdc22b923ddb73c605239
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-14 20:14:27 +0900

    꿍가라사대_11094 IO swift 문제풀이

---
+++ BaekJoon/IO/꿍가라사대_11094.swift
@@ -0,0 +1,26 @@
+//
+//  꿍가라사대_11094.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/02/14.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: 문제풀이
+/*
+import Foundation
+
+let T = Int(readLine()!)!
+
+for _ in 0..<T {
+    let senten = readLine()!.split(separator: " ").map { String($0) }
+    if senten[0] != "Simon" || senten[1] != "says" { continue }
+    
+    print(terminator: " ")
+    for i in 2...senten.count-1 {
+        print(senten[i], terminator: " ")
+    }
+    print()
+    
+}
+*/
