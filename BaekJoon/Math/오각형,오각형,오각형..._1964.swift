5d82e87c02df29026753156a760ff87ac3b6e3ce
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-14 20:05:17 +0900

    오각형,오각형,오각형..._1964 swift 문제풀이

---
+++ BaekJoon/Math/오각형,오각형,오각형..._1964.swift
@@ -0,0 +1,23 @@
+//
+//  오각형,오각형,오각형..._1964.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/02/14.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: Math 문제풀이
+/*
+import Foundation
+
+var Ans = 5
+let N = Int(readLine()!)!
+
+if N == 1 { print(Ans); exit(0) }
+
+for i in 2...N {
+    Ans += (i * 3 + 1)
+}
+
+print(Ans % 45678)
+*/
