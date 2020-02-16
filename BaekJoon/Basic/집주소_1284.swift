895a490085407a47db94dc66a5b9d24099fb4e5e
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-16 13:18:48 +0900

    집주소_1284 swift 문제풀이

---
+++ BaekJoon/Basic/집주소_1284.swift
@@ -0,0 +1,28 @@
+//
+//  집주소_1284.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/02/16.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 집주소
+// MARK: 문제풀이
+/*
+import Foundation
+
+while true {
+    var N = Int(readLine()!)!
+    var Ans = 1
+    if N == 0 { break }
+    while N > 0 {
+        let r = N % 10
+        if r == 0 { Ans += 4 }
+        else if r == 1 { Ans += 2 }
+        else { Ans += 3 }
+        Ans += 1
+        N /= 10
+    }
+    print(Ans)
+}
+*/
