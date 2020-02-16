d6f2f6dec69492f3e7e4282e9971b547b993574e
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-16 14:23:03 +0900

    정수제곱근_2417 swift 문제풀이

--- BaekJoon/Math/달달함이넘쳐흘러_17256.swift
+++ BaekJoon/Math/달달함이넘쳐흘러_17256.swift
@@ -6,4 +6,12 @@
 //  Copyright © 2020 Min Kyeong Tae. All rights reserved.
 //
 
+// MARK: - 달달함이넘쳐흘러 17256
+// MARK: 문제풀이
+/*
 import Foundation
+
+let a = readLine()!.split(separator: " ").map { Int($0)! }
+let c = readLine()!.split(separator: " ").map { Int($0)! }
+print("\(c[0]-a[2]) \(c[1]/a[1]) \(c[2]-a[0])")
+*/
---
+++ BaekJoon/Math/정수제곱근_2417.swift
@@ -0,0 +1,16 @@
+//
+//  정수제곱근_2417.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/02/16.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 정수 제곱근 2417
+// MARK: 문제풀이
+/*
+import Foundation
+
+let N = Int(readLine()!)!
+print(Int(ceil(sqrt(Double(N)))))
+*/
