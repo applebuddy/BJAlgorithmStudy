90d7a22d8cce26aacb2444afb1d1214d5bb2dd80
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-16 14:28:18 +0900

    ShiftySum_14682 swift 문제풀이

---
+++ BaekJoon/Math/ShiftySum.swift
@@ -0,0 +1,24 @@
+//
+//  ShiftySum.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/02/16.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - Shifty Sum 14682
+// MARK: 문제풀이
+/*
+import Foundation
+
+var N = Int(readLine()!)!
+let K = Int(readLine()!)!
+var Ans = 0
+
+for _ in 0...K {
+    Ans += N
+    N *= 10
+}
+
+print(Ans)
+*/
