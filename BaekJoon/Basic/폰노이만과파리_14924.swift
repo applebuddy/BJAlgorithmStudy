16dd4fd4847bde5cd2f96f979e157394a473deda
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-16 13:47:58 +0900

    폰노이만과파리_14924 swift 문제풀이

---
+++ BaekJoon/Basic/폰노이만과파리_14924.swift
@@ -0,0 +1,16 @@
+//
+//  폰노이만과파리_14924.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/02/16.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 폰노이만과파리
+// MARK: 문제풀이
+/*
+import Foundation
+
+let arr = readLine()!.split(separator: " ").map { Int($0)! }
+print(arr[2] / (2 * arr[0]) * arr[1])
+*/
