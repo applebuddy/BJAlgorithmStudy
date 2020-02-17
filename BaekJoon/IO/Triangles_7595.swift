8bbb7ef476d69e27334a83bf3ab5271ef9956f00
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-17 22:03:02 +0900

    triangles_7595 IO swift 문제풀이

---
+++ BaekJoon/IO/Triangles_7595.swift
@@ -0,0 +1,24 @@
+//
+//  Triangles_7595.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/02/17.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - Triangles
+// MARK: 문제풀이
+/*
+ 
+import Foundation
+
+while true {
+    let N = Int(readLine()!)!
+    if N == 0 { break }
+    
+    for i in 1...N {
+        print([String](repeating: "*", count: i).joined())
+    }
+}
+ 
+*/
