014569ef6f74c5941b46cffa583ef823b25e15c9
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-17 21:03:54 +0900

    수들의합_2018 Math swift 문제풀이

---
+++ BaekJoon/Math/수들의합_2018.swift
@@ -0,0 +1,27 @@
+//
+//  수들의합_2018.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/02/17.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 수들의 합 5
+// MARK: 문제풀이
+/*
+import Foundation
+
+let N = Int(readLine()!)!
+var Ans = 0
+
+for i in 1...N {
+    var sum = 0
+    for j in i...N {
+        sum += j
+        if sum > N { break }
+        if sum == N { Ans += 1; break }
+    }
+}
+
+print(Ans)
+*/
