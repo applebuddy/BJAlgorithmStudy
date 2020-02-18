a87a330b2d81e3ede912323630942aed631cb9b6
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-18 23:01:49 +0900

    팰린드롬_13235 swift 문제풀이

---
+++ BaekJoon/String/팰린드롬_13235.swift
@@ -0,0 +1,16 @@
+//
+//  팰린드롬_13235.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/02/18.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 팰린드롬_13235
+// MARK: 문제풀이
+/*
+import Foundation
+
+let str = Array(readLine()!)
+print(str == str.reversed() ? "true" : "false")
+*/
