6194b9d88adbdc500473bc7368a52e312d036195
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-26 03:07:22 +0900

    AC_5430 Deque C++ 문제풀이

---
+++ BaekJoon/Queue/Deque/AC_5430.cpp
@@ -0,0 +1,72 @@
+
+//
+//  AC_5430.cpp
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/02/26.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - AC 5430
+// MARK: Deque C++ 문제풀이
+
+#if 0
+#include <iostream>
+#include <string>
+#include <deque>
+using namespace std;
+
+int main() {
+    ios_base :: sync_with_stdio(0); cin.tie(0);
+    int T; cin>>T;
+    
+    while(T--) {
+        deque<string> DQ;
+        string code; cin>>code;
+        int len; cin>>len;
+        string arr; cin>>arr;
+        string temp = "";
+        for(int i=0; i<arr.length(); i++) {
+            if(i==0 || i==arr.length()-1) continue;
+            if(arr[i] == ',') {
+                DQ.push_back(temp);
+                temp = "";
+            } else temp += arr[i];
+        }
+        if(temp != "") DQ.push_back(temp);
+        
+        bool isReverse = false;
+        bool isError = false;
+        for(int i=0; i<code.length(); i++) {
+            if(code[i]=='D') {
+                if(DQ.empty()) {
+                    isError = true;
+                    break;
+                }
+                if(isReverse) DQ.pop_back();
+                else DQ.pop_front();
+            } else {
+                isReverse = !isReverse;
+            }
+        }
+        
+        if(isError) printf("error\n");
+        else {
+            string Ans = "[";
+            while(!DQ.empty()) {
+                if(isReverse) {
+                    Ans += DQ.back();
+                    DQ.pop_back();
+                } else {
+                    Ans += DQ.front();
+                    DQ.pop_front();
+                }
+                if(!DQ.empty()) Ans += ',';
+            }
+            Ans += "]";
+            printf("%s\n",Ans.c_str());
+        }
+    }
+    return 0;
+}
+#endif
---
+++ BaekJoon/Queue/Deque/AC_5430.swift
@@ -0,0 +1,58 @@
+//
+//  AC_5430.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/02/26.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: TLE 답안
+/*
+import Foundation
+
+let N = Int(readLine()!)!
+
+for _ in 0..<N {
+    let fuc = Array(readLine()!)
+    let _ = readLine()
+    var arr2 = Array(readLine()!)
+    arr2.removeLast()
+    arr2.removeFirst()
+    var nArr = arr2.split(separator: ",")
+    
+    var flag = true
+    var isReversed = false
+    for i in fuc.indices {
+        if nArr.isEmpty { flag = false; break }
+        if fuc[i] == "D" {
+            if isReversed {
+                nArr.removeLast()
+            } else {
+                nArr.removeFirst()
+            }
+        } else {
+            isReversed = !isReversed
+        }
+    }
+    
+    if !flag { print("error") }
+    else {
+        var str = "["
+        if isReversed {
+            var idx = nArr.count - 1
+            while idx >= 0 {
+                str += "\(Int(String(nArr[idx]))!)"
+                if idx != 0 { str += "," }
+                idx -= 1
+            }
+        } else {
+            for i in nArr.indices {
+                str += "\(Int(String(nArr[i]))!)"
+                if i != nArr.count-1 { str += "," }
+            }
+        }
+        str += "]"
+        print(str)
+    }
+}
+*/
