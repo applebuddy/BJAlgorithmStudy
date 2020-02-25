1e3144a7951fc1dc51aebc82f3f0e74c267d9411
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-25 23:47:54 +0900

    숫자카드2_10816 Hash swift 문제풀이

---
+++ BaekJoon/Searching/Hash/숫자카드2_10816.swift
@@ -0,0 +1,32 @@
+//
+//  숫자카드2_10816.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/02/25.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 숫자카드2 10816
+// MARK: Hash swift 문제풀이
+
+/*
+import Foundation
+
+let N = readLine()
+let arr = readLine()!.split(separator: " ").map { String($0) }
+let M = readLine()
+var dic = [String:Int]()
+
+let arr2 = readLine()!.split(separator: " ").map { String($0) }
+for i in arr.indices {
+    dic[arr[i]] = (dic[arr[i]] ?? 0) + 1
+}
+
+var Ans = ""
+for i in arr2.indices {
+    if dic[arr2[i]] == nil { Ans += "\(0) " }
+    else { Ans += "\(dic[arr2[i]]!) " }
+}
+
+print(Ans)
+*/
