a51fd555c3048e6be3bf5b4358f7142c58babc66
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-28 17:42:52 +0900

    교수가된현우_3474 Math swift 문제풀이

---
+++ BaekJoon/Math/교수가된현우_3474.swift
@@ -0,0 +1,26 @@
+//
+//  교수가된현우_3474.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/02/28.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 교수가 된 현우 3474
+// MARK: Math 문제풀이
+
+/*
+import Foundation
+
+let T = Int(readLine()!)!
+for _ in 0..<T {
+    let N = Int(readLine()!)!
+    var K = 5
+    var Ans = 0
+    while K <= N {
+        Ans += N / K
+        K *= 5
+    }
+    print(Ans)
+}
+*/
