4adc09a416d78e14322a8e7b81fba6868d18a513
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-28 16:56:27 +0900

    분해합_2231 Math swift 문제풀이

---
+++ BaekJoon/Math/분해합_2231.swift
@@ -0,0 +1,29 @@
+//
+//  분해합_2231.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/02/28.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 분해합 2231
+// MARK: swift 문제풀이
+
+/*
+import Foundation
+
+let N = Int(readLine()!)!
+
+for i in 1...N {
+    var num = i
+    var sum = i
+    while num > 0 {
+        sum += num % 10
+        num /= 10
+    }
+    
+    if sum == N { print(i); exit(0) }
+}
+
+print(0)
+*/
