cb233e5aa1c2f211c0fea5eed9845e067d3da258
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-29 03:16:14 +0900

    큰수만들기_16496 greedy swift 문제풀이

--- BaekJoon/Greedy/기타줄_1049.swift
+++ BaekJoon/Greedy/기타줄_1049.swift
@@ -9,21 +9,19 @@
 // MARK: 기타줄_1049
 
 // MARK: - 문제풀이
+
 /*
-import Foundation
+ import Foundation
 
-let arr = readLine()!.split(separator: " ").map { Int($0)! }
-let N = arr[0], B = arr[1]
-var P: (Int, Int) = (1234, 1234)
-for _ in 0..<B {
-    let arr2 = readLine()!.split(separator: " ").map { Int($0)! }
-    let a = arr2[0], b = arr2[1]
-    P.0 = min(P.0, a)
-    P.1 = min(P.1, b)
-}
+ let arr = readLine()!.split(separator: " ").map { Int($0)! }
+ let N = arr[0], B = arr[1]
+ var P: (Int, Int) = (1234, 1234)
+ for _ in 0..<B {
+ let arr2 = readLine()!.split(separator: " ").map { Int($0)! }
+ let a = arr2[0], b = arr2[1]
+ P.0 = min(P.0, a)
+ P.1 = min(P.1, b)
+ }
 
-print(min(P.0 * (N/6) + P.1 * (N%6), min(P.0 * ((N-1)/6+1), P.1 * N)))
-*/
-
-
-
+ print(min(P.0 * (N/6) + P.1 * (N%6), min(P.0 * ((N-1)/6+1), P.1 * N)))
+ */
---
+++ BaekJoon/Greedy/큰수만들기_16496.swift
@@ -0,0 +1,23 @@
+//
+//  큰수만들기_16496.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/02/29.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 큰 수 만들기 16496
+// MARK: Greedy swift 문제풀이
+
+/*
+import Foundation
+
+let N = Int(readLine()!)!
+var arr = readLine()!.split(separator: " ").map { String($0) }
+arr.sort {
+    "\($0)\($1)" > "\($1)\($0)"
+}
+
+if arr.first! == "0" { print(0); }
+else { print(arr.joined()) }
+*/
