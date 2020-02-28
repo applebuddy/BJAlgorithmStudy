ab4ce67e2e36153da6b8784b09a54c122bf7e5cb
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-28 20:03:39 +0900

    별찍기-21_10996 swift 문제풀이

---
+++ BaekJoon/IO/별찍기-21_10996.swift
@@ -0,0 +1,25 @@
+//
+//  별찍기-21_10996.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/02/28.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 별찍기-21 10996
+// MARK: IO swift 문제풀이
+
+/*
+import Foundation
+
+let N = Int(readLine()!)!
+if N == 1 { print("*"); exit(0) }
+for i in 0..<N*2 {
+    var S = ""
+    for j in 0..<N {
+        if (i & 1 == 0 && j & 1 == 0) || (i & 1 == 1 && j & 1 == 1) { S += "*" }
+        else { S += " " }
+    }
+    print(S)
+}
+*/
