44d7bf6a34a94c3b796529d1491a0f8bd21fc407
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-02 03:50:59 +0900

    팰린드롬만들기_1213 string swift 문제풀이

--- BaekJoon/String/팰린드롬만들기_1213.swift
+++ BaekJoon/String/팰린드롬만들기_1213.swift
@@ -2,37 +2,87 @@
 //  팰린드롬만들기_1213.swift
 //  BackJoonPractice
 //
-//  Created by MinKyeongTae on 2020/02/09.
+//  Created by MinKyeongTae on 2020/03/02.
 //  Copyright © 2020 Min Kyeong Tae. All rights reserved.
 //
 
-import Foundation
+// MARK: - 팰린드롬 만들기 1212
+
+// MARK: string swift 문제풀이2 (좀 더 간결한 버전)
 
 /*
- import Foundation
+import Foundation
 
- let G = Array(readLine()!)
- var dic = [Character: Int]()
- for i in G.indices {
-     dic[G[i]] = (dic[G[i]] ?? 0) + 1
- }
+let S = Array(readLine()!).sorted()
+let len = S.count
+var dic = [Character:Int]()
 
- var chk = dic.sorted {
-     $0.value > $1.value || ($0.value == $1.value && $0.key < $1.key )
- }
- var chk2 = [String]()
+for i in S.indices {
+    dic[S[i]] = (dic[S[i]] ?? 0) + 1
+}
 
- print(chk)
+var idx = 0
+var L = ""
+var R = ""
+var center = ""
+while idx < len {
+    var cnt = dic[S[idx]]!
+    let next = idx + cnt
+    if cnt % 2 == 0 {
+            L = "\(L)\([String](repeating: "\(S[idx])", count: cnt/2).joined())"
+            R = "\([String](repeating: "\(S[idx])", count: cnt/2).joined())\(R)"
+    } else {
+        cnt -= 1
+        if center != "" { print("I'm Sorry Hansoo"); exit(0) }
+        else {
+            center = "\(S[idx])"
+            L = "\(L)\([String](repeating: "\(S[idx])", count: cnt/2).joined())"
+            R = "\([String](repeating: "\(S[idx])", count: cnt/2).joined())\(R)"
+        }
+    }
+    idx = next
+}
 
- for i in chk.indices {
-     chk2.append(String(chk[i].0))
- }
+print("\(L)\(center)\(R)")
+*/
 
- if chk.count == 2 && chk.first!.1 == 2 {
-     print("\(chk2.joined())\(chk2.reversed().joined())")
- } else {
-     print("\(chk2.joined())\(chk2.reversed().joined())")
-     
- }
+// MARK: string swift 문제풀이
 
- */
+/*
+import Foundation
+
+let S = Array(readLine()!).sorted()
+var dic = [Character:Int]()
+
+for i in S.indices {
+    dic[S[i]] = (dic[S[i]] ?? 0) + 1
+}
+
+var idx = 0
+var L = ""
+var R = ""
+var center = ""
+while idx < S.count {
+    var cnt = dic[S[idx]]!
+    let next = idx + cnt
+    if cnt % 2 == 0 {
+        for _ in 0..<cnt/2 {
+            L = "\(L)\(S[idx])"
+            R = "\(S[idx])\(R)"
+        }
+    } else {
+        cnt -= 1
+        if center != "" { print("I'm Sorry Hansoo"); exit(0) }
+        else {
+            center = "\(S[idx])"
+            for _ in 0..<cnt/2 {
+                L = "\(L)\(S[idx])"
+                R = "\(S[idx])\(R)"
+            }
+        }
+    }
+    idx = next
+}
+
+print("\(L)\(center)\(R)")
+*/
