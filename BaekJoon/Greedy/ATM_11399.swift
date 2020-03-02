7e33b519d01fe09303e0c56fb5ba055faa824261
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-02 16:46:04 +0900

    ATM_11399 greedy swift 문제풀이

---
+++ BaekJoon/Greedy/ATM_11399.swift
@@ -0,0 +1,25 @@
+//
+//  ATM_11399.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/03/02.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - ATM 11399
+// MARK: greedy swift 문제풀이
+
+/*
+import Foundation
+
+let N = Int(readLine()!)!
+var arr = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
+var sum = arr[0]
+var Ans = sum
+for i in 1..<arr.count {
+    sum += arr[i]
+    Ans += sum
+}
+
+print(Ans)
+*/
