6149b662543c8b317e89a8f0df5ce9fefde3aa9b
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-02 18:13:55 +0900

    거꾸로구구단_13410 swift 문제풀이

---
+++ BaekJoon/Basic/거꾸로구구단_13410.swift
@@ -0,0 +1,31 @@
+//
+//  거꾸로구구단_13410.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/03/02.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 거꾸로 구구단 13410
+// MARK: swift 문제풀이
+
+/*
+import Foundation
+
+let arr = readLine()!.split(separator: " ").map { Int($0)! }
+let N = arr[0], K = arr[1]
+var Ans = 0
+
+for i in 1...K {
+    var num = N * i
+    var reverse = ""
+    while num > 0 {
+        reverse = "\(reverse)\(num%10)"
+        num /= 10
+    }
+    let rev = Int(reverse)!
+    Ans = Ans < rev ? rev : Ans
+}
+
+print(Ans)
+*/
--- BaekJoon/Basic/숫자카드2_10816.swift
+++
@@ -1,9 +0,0 @@
-//
-//  숫자카드2_10816.swift
-//  BackJoonPractice
-//
-//  Created by MinKyeongTae on 2020/02/25.
-//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
-//
-
-import Foundation
