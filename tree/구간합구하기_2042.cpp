194832ced570472e37670bb6db3f4d95c0db2b5a
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-05 20:02:08 +0900

    구간합구하기_2042 segmentTree 학습 & C++ 문제풀이기록

---
+++ tree/구간합구하기_2042.cpp
@@ -0,0 +1,9 @@
+//
+//  구간합구하기_2042.cpp
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/03/05.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+#include <stdio.h>
