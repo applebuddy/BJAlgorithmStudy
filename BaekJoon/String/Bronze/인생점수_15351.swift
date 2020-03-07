887e1bb4c9a7e12dee2edf0570199c3cd5a9211b
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-07 18:37:14 +0900

    인생점수_15351 swift 문제풀이

---
+++ BaekJoon/String/Bronze/인생점수_15351.swift
@@ -0,0 +1,25 @@
+//
+//  인생점수_15351.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/03/07.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 인생점수 15351
+// MARK: swift 문제풀이
+
+/*
+import Foundation
+
+let N = Int(readLine()!)!
+for _ in 0..<N {
+    let arr = Array(readLine()!)
+    var score = 0
+    for i in arr.indices {
+        if arr[i] == " " { continue }
+        score += Int(arr[i].unicodeScalars.first!.value) - 64
+    }
+    print(score == 100 ? "PERFECT LIFE" : "\(score)")
+}
+*/
