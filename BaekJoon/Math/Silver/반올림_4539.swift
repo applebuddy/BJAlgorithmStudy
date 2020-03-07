22485d8cc129eb61778235cc46375f9d9830c51c
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-07 18:45:50 +0900

    반올림_4539 math swift 문제풀이

---
+++ BaekJoon/Math/Silver/반올림_4539.swift
@@ -0,0 +1,25 @@
+//
+//  반올림_4539.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/03/07.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 반올림
+// MARK: swift 문제풀이
+
+/*
+import Foundation
+
+let T = Int(readLine()!)!
+for _ in 0..<T {
+    var N = Double(readLine()!)!
+    var target: Double = 1
+    while N > target {
+        N = (N/target).rounded()*target
+        target *= 10
+    }
+    print(Int(N))
+}
+*/
