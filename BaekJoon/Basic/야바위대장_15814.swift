4a5bd3dda4e41562a0180a8341d79d9f673032a1
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-07 17:18:19 +0900

    야바위대장_15814 swift 문제풀이

---
+++ BaekJoon/Basic/야바위대장_15814.swift
@@ -0,0 +1,24 @@
+//
+//  야바위대장_15814.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/03/07.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 야바위대장 15814
+// MARK: swift 문제풀이
+
+/*
+import Foundation
+
+var S = Array(readLine()!)
+let K = Int(readLine()!)!
+for _ in 0..<K {
+    let arr = readLine()!.split(separator: " ").map { Int($0)! }
+    let from = arr[0], to = arr[1]
+    S.swapAt(from, to)
+}
+
+print(S.map { String($0) }.joined())
+*/
