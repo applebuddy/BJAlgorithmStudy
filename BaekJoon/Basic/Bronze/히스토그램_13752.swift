27a7e7003a0b580ee25f6500652ee7011875e0d8
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-09 03:08:30 +0900

    히스토그램_13752 swift 문제풀이

---
+++ BaekJoon/Basic/Bronze/히스토그램_13752.swift
@@ -0,0 +1,20 @@
+//
+//  히스토그램_13752.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/03/09.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 히스토그램 13752
+// MARK: 히스토그램은 데이터를 시각적으로 표현한 것이다. 막대로 구성되어진 히스토그램을 출력하는 문제
+
+/*
+import Foundation
+
+let T = Int(readLine()!)!
+for _ in 0..<T {
+    let N = Int(readLine()!)!
+    print(String(repeating: "=", count: N))
+}
+*/
