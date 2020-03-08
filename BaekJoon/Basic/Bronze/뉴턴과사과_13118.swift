b09e9e3b3dfd079fc8a0c21375017da28e13347d
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-08 20:45:51 +0900

    뉴턴과사과_13118 swift 문제풀이

---
+++ BaekJoon/Basic/Bronze/뉴턴과사과_13118.swift
@@ -0,0 +1,23 @@
+//
+//  뉴턴과사과_13118.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/03/08.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 뉴턴과 사과 13118
+// MARK: swift 문제풀이
+
+/*
+import Foundation
+
+let arr = readLine()!.split(separator: " ").map { Int($0)! }
+let arr2 = readLine()!.split(separator: " ").map { Int($0)! }
+let X = arr2[0]
+for i in arr.indices {
+    if arr[i] == X { print(i+1); exit(0) }
+}
+
+print(0)
+*/
