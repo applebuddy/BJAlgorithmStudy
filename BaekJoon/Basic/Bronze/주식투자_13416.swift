9d59d958c608caafb511e6f3d923411c11953fcd
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-09 20:13:39 +0900

    주식투자_13416 swift 문제풀이

---
+++ BaekJoon/Basic/Bronze/주식투자_13416.swift
@@ -0,0 +1,24 @@
+//
+//  주식투자_13416.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/03/09.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 주식투자_13416
+// MARK: swift 문제풀이
+
+/*
+let T = Int(readLine()!)!
+
+for _ in 0..<T {
+    let N = Int(readLine()!)!
+    var Ans = 0
+    for _ in 0..<N {
+        let earn = readLine()!.split(separator: " ").map { Int($0)! }.max()!
+        Ans += earn > 0 ? earn : 0
+    }
+    print(Ans)
+}
+*/
