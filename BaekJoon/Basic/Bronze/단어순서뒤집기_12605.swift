8c96ed4faa12aae223526bd5f3e1d70bd32961aa
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-09 22:11:59 +0900

    단어순서뒤집기_12605 swift 문제풀이

---
+++ BaekJoon/Basic/Bronze/단어순서뒤집기_12605.swift
@@ -0,0 +1,21 @@
+//
+//  단어순서뒤집기_12605.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/03/09.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 단어순서 뒤집기 12605
+// MARK: swift 문제풀이
+
+/*
+import Foundation
+
+let T = Int(readLine()!)!
+
+for i in 1...T {
+    let arr = readLine()!.split(separator: " ").reversed()
+    print("Case #\(i): \(arr.joined(separator: " "))")
+}
+*/
