593ce2031986ac2baa7fd24ed8e38c3d77700ac1
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-09 18:48:26 +0900

    Time-to-Decompress swift 문제풀이

---
+++ BaekJoon/Basic/Bronze/Time-to-Decompress_17010.swift
@@ -0,0 +1,20 @@
+//
+//  Time-to-Decompress_17010.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/03/09.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - Time-to-Decompress
+// MARK: swift 문제풀이
+
+/*
+import Foundation
+
+let T = Int(readLine()!)!
+for _ in 0..<T {
+    let arr = readLine()!.split(separator: " ")
+    print(String(repeating: String(arr[1]), count: Int(arr[0])!))
+}
+*/
