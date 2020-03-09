6b534dabd1b5ef9a74a887218f48354032f875ea
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-09 19:33:42 +0900

    관리자는누구?_14724 swift 문제풀이

---
+++ BaekJoon/Basic/Bronze/관리자는누구?_14724.swift
@@ -0,0 +1,28 @@
+//
+//  관리자는누구?_14724.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/03/09.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 관리자는 누구? 14724
+// MARK: swift 문제풀이
+
+/*
+import Foundation
+
+let _ = readLine()
+
+let group = ["PROBRAIN", "GROW", "ARGOS", "ADMIN", "ANT", "MOTION", "SPG", "COMON", "ALMIGHTY"]
+var Ans = 0, maxVal = 0
+
+for i in 0..<9 {
+    let val = readLine()!.split(separator: " ").map { Int($0)! }.max()!
+    if maxVal < val {
+        maxVal = val; Ans = i
+    }
+}
+
+print(group[Ans])
+*/
