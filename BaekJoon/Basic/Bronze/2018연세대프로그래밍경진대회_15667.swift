d3baa4c897061c2b7597ef80d996958b54520a11
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-09 19:07:59 +0900

    2018-연세대-프로그래밍-경진대회_15667 swift 문제풀이

---
+++ BaekJoon/Basic/Bronze/2018연세대프로그래밍경진대회_15667.swift
@@ -0,0 +1,18 @@
+//
+//  2018연세대프로그래밍경진대회_15667.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/03/09.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: swift 문제풀이
+
+/*
+import Foundation
+
+let N = Int(readLine()!)!
+for i in 1...100 {
+    if 1 + i + i*i == N { print(i); exit(0) }
+}
+*/
