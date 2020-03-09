3ddc8e8b2a66cc3036eae1d2754f85beee99eed5
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-09 20:04:06 +0900

    Quick-Estimates_13297 swift 문제풀이

---
+++ BaekJoon/Basic/Bronze/Quick-Estimates_13297.swift
@@ -0,0 +1,15 @@
+//
+//  Quick-Estimates_13297.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/03/09.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - Quick-Estimates
+// MARK: swift 문제풀이
+
+/*
+let T = Int(readLine()!)!
+for _ in 0..<T { print(Array(readLine()!).count) }
+*/
