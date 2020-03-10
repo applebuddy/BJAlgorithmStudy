0198572aa13dd933da7c807a14184ec5c3ed884b
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-10 20:20:54 +0900

    우유축제_14720 swift 문제풀이

---
+++ BaekJoon/Math/Bronze/우유축제_14720.swift
@@ -0,0 +1,23 @@
+//
+//  우유축제_14720.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/03/10.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 우유축제_14720
+// MARK: swift 문제풀이
+
+/*
+let _ = readLine()
+let arr = readLine()!.split(separator: " ").map { Int($0)! }
+var milk = 0
+var Ans = 0
+for i in arr.indices {
+    if milk == arr[i] { Ans += 1; milk += 1 }
+    if milk > 2 { milk = 0 }
+}
+
+print(Ans)
+*/
