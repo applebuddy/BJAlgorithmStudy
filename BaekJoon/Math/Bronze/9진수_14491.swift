db061e81e8cd4cc714fa1d241673c56c098c0361
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-10 20:20:28 +0900

    9진수_14491 swift 문제풀이

---
+++ BaekJoon/Math/Bronze/9진수_14491.swift
@@ -0,0 +1,21 @@
+//
+//  9진수_14491.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/03/10.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 9진수 14491
+// MARK: swift 문제풀이
+
+/*
+var N = Int(readLine()!)!
+var Ans = ""
+while N > 0 {
+    Ans = "\(N % 9)\(Ans)"
+    N /= 9
+}
+
+print(Ans)
+*/
