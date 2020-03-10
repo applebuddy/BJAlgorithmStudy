b86e16e4a2fd0b78c2dff7a4d734fa3a15603044
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-10 20:07:37 +0900

    3n+1수열_14920 math swift 문제풀이

---
+++ BaekJoon/Math/Bronze/3n+1수얼_14920.swift
@@ -0,0 +1,23 @@
+//
+//  3n+1수얼_14920.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/03/10.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 3n+1 수열
+// MARK: swift 문제풀이
+
+/*
+var N = Int(readLine()!)!
+var Ans = 1
+
+while true {
+    if N == 1 { break }
+    N = N & 1 != 1 ? N/2 : 3*N+1
+    Ans += 1
+}
+
+print(Ans)
+*/
