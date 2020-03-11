169d07bf21cb69096ec12ffd97dcd7144e06e0d9
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-12 03:59:21 +0900

    전자레인지_10162 swift 문제풀이

---
+++ BaekJoon/Basic/Bronze/전자레인지_10162.swift
@@ -0,0 +1,14 @@
+//
+//  전자레인지_10162.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/03/12.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 전자레인지
+// MARK: swift 숏코딩 문제풀이
+/*
+import Foundation
+let A=300,B=60,C=10;var N=Int(readLine()!)!;if N%10 != 0{print(-1);exit(0)};var Ans=[0,0,0];Ans[0]=N/A;N%=A;Ans[1]=N/B;N%=B;Ans[2]=N/C;print(Ans.map{String($0)}.joined(separator:" "))
+*/
