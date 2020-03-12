0a23f4e947eb36fdd4bca758fa8c8efb0d968633
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-12 18:40:42 +0900

    토마토_7576 BFS DoubleStackQueue 활용 swift 문제풀이

--- BaekJoon/Graph/토마토_7576.swift
+++ BaekJoon/Graph/토마토_7576.swift
@@ -7,6 +7,85 @@
 //
 
 // MARK: - 토마토 7576
+
+// MARK: stackQueue 활용 답안
+/*
+import Foundation
+
+let arr = readLine()!.split(separator: " ").map { Int($0)! }
+let M = arr[0], N = arr[1]
+var G = [[Int]](repeating: [Int](), count: N)
+var Ans = 0
+let dx = [0,0,1,-1]
+let dy = [1,-1,0,0]
+
+struct DoubleStackQueue<T> {
+    var leftStack = [T]()
+    var rightStack = [T]()
+    
+    mutating func push(_ val: T) {
+        rightStack.append(val)
+    }
+    
+    mutating func pop() {
+        if leftStack.isEmpty {
+            leftStack = rightStack.reversed()
+            rightStack.removeAll()
+        }
+        leftStack.removeLast()
+    }
+    
+    mutating func front() -> T? {
+        return !leftStack.isEmpty ? leftStack.last! : rightStack.first!
+    }
+    
+    func empty() -> Bool {
+        return leftStack.isEmpty && rightStack.isEmpty
+    }
+}
+
+for i in 0..<N {
+    G[i] = readLine()!.split(separator: " ").map { Int($0)! }
+}
+
+var DQ = DoubleStackQueue<(Int,Int)>()
+var rem = 0
+
+for i in G.indices {
+    for j in G[i].indices {
+        if G[i][j] == 1 { DQ.push((i,j)) }
+        else if G[i][j] == 0 { rem += 1 }
+    }
+}
+
+if rem == 0 { print(0); exit(0) }
+
+func BFS() {
+    while !DQ.empty() {
+        let nowQ = DQ.front()!
+        let x = nowQ.0
+        let y = nowQ.1
+        DQ.pop()
+        for i in 0..<4 {
+            let nx = x + dx[i]
+            let ny = y + dy[i]
+            if nx < 0 || ny < 0 || nx >= N || ny >= M { continue }
+            if G[nx][ny] != 0 { continue }
+            G[nx][ny] = G[x][y] + 1
+            Ans = Ans < G[nx][ny] ? G[nx][ny] : Ans
+            rem -= 1
+            if rem <= 0 { break }
+            DQ.push((nx,ny))
+        }
+    }
+}
+
+BFS()
+
+if rem > 0 { print(-1) }
+else { print(Ans-1) }
+*/
+
 // MARK: cursored Queue 활용 답안
 
 /*
