8b2b1a073e7faad5b8167e0446bb36f6cf7eb3a0
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-13 17:59:33 +0900

    Yangjojang-of-The-Year_11557 swift 문제풀이

---
+++ BaekJoon/Sorting/Yangjojang-of-The-Year_11557.swift
@@ -0,0 +1,19 @@
+
+// MARK: - Yangjojang-of-The-Year 11557
+// MARK: swift 문제풀이
+/*
+let T = Int(readLine()!)!
+
+for _ in 0..<T {
+    let N = Int(readLine()!)!
+    var univ = [(String, Int)]()
+    for _ in 0..<N {
+        let arr = readLine()!.split(separator: " ")
+        let pair = (String(arr[0]), Int(arr[1])!)
+        univ.append(pair)
+    }
+    univ.sort { $0.1 > $1.1 }
+    print(univ.first!.0)
+}
+*/
+
