f5c12b8a47003410a360efe9ba51429715f87120
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-13 16:54:34 +0900

    Presents_13771 swift 문제풀이

---
+++ BaekJoon/Sorting/Presents_13771.swift
@@ -0,0 +1,17 @@
+
+// MARK: - Presents
+// MARK: swift 문제풀이
+/*
+let N = Int(readLine()!)!
+var score = [Double]()
+for _ in 0..<N {
+    let val = Double(readLine()!)!
+    if val > 100 { continue }
+    score.append(val)
+}
+
+score.sort()
+
+let rounded = (score[1]*10).rounded() / 10
+print(score[1] == rounded ? "\(score[1])0" : "\(score[1])")
+*/
