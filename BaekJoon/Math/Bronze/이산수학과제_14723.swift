f23406fda4545cf4c024131780eb83fac28a9030
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-13 18:31:30 +0900

    이산수학과제_14723 math swift 문제풀이

---
+++ BaekJoon/Math/Bronze/이산수학과제_14723.swift
@@ -0,0 +1,21 @@
+
+// MARK: - 이산수학 과제 14723
+// MARK: swift 문제풀이
+
+/*
+let N = Int(readLine()!)!
+var target = 1
+var now = 0
+var Ans = ["", ""]
+while true {
+    now += target
+    if now >= N {
+        Ans[0] = "\(now-N+1)"
+        Ans[1] = "\(target-(now-N))"
+        break
+    }
+    target += 1
+}
+
+print(Ans.joined(separator: " "))
+*/
