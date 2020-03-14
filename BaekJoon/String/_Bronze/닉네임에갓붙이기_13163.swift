41ffecfd9f25f1e8438396556fb30d6e93df8a68
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-15 00:38:48 +0900

    닉네임에갓붙이기_13163 swift 문제풀이

---
+++ BaekJoon/String/_Bronze/닉네임에갓붙이기_13163.swift
@@ -0,0 +1,12 @@
+
+// MARK: - 닉네임에 갓 붙이기 13163
+// MARK: swift 문제풀이
+
+/*
+let N = Int(readLine()!)!
+for _ in 0..<N {
+    var arr = readLine()!.split(separator: " ").map { String($0) }
+    arr[0] = "god"
+    print(arr.joined())
+}
+*/
