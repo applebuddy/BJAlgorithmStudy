b7852cfc4449a777a8f51cd492a90c5713220234
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-15 01:49:27 +0900

    개수세기_10807 swift 문제풀이

---
+++ BaekJoon/구현/_Bronze/개수세기_10807.swift
@@ -0,0 +1,7 @@
+
+// MARK: - 개수세기 10807
+// MARK: 숏코딩 swift 문제풀이
+
+/*
+var Ans=0;let N=Int(readLine()!)!;let arr=readLine()!.split(separator:" ").map{Int($0)!};let K=Int(readLine()!)!;for i in arr.indices{Ans+=arr[i]==K ?1:0};print(Ans)
+*/
