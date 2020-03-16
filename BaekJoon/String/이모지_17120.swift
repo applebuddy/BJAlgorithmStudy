ffd9cbddd0027ee5d4b46937e31f160944c8b503
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-17 03:18:51 +0900

    이모지_17120 contains() 활용 swift 문제풀이

---
+++ BaekJoon/String/이모지_17120.swift
@@ -0,0 +1,13 @@
+
+// MARK: - 이모지 17120
+// ★ contains() 를 통해 subString을 찾고자 한다면 Foundation Framework를 반드시 import하고 사용하자!!
+
+/*
+import Foundation
+
+while let S = readLine() {
+    if S.contains(":cat:") {
+        print("YES")
+    } else { print("NO") }
+}
+*/
