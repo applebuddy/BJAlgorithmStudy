579b77d942cff6d0c4957b824a72829719430d5c
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-18 04:00:43 +0900

    사분면고르기_146811 math swift 숏코딩 문제풀이

---
+++ BaekJoon/Math/_Bronze/사분면고르기_14681.swift
@@ -0,0 +1,7 @@
+
+// MARK: - 사분면고르기_14681
+// MARK: math swift 숏코딩 문제풀이
+
+/*
+let X=Int(readLine()!)!;let Y=Int(readLine()!)!;var Ans=1;if X>0&&Y>0{Ans=1}else if X<0&&Y>0{Ans=2}else if X<0&&Y<0{Ans=3}else{Ans=4};print(Ans)
+*/
