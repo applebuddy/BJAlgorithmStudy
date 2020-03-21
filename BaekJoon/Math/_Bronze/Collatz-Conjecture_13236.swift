47a7519d353771578514b5a002bca672b00d922c
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-21 15:58:18 +0900

    Collatz-Conjecture_13236 math swift 문제풀이

---
+++ BaekJoon/Math/_Bronze/Collatz-Conjecture_13236.swift
@@ -0,0 +1,19 @@
+
+// MARK: - Collatz Conjecture 13236
+// MARK: math swift 문제풀이
+
+/*
+var N = Int(readLine()!)!
+var Ans = [String]()
+while true {
+    Ans.append("\(N)")
+    if N == 1 { break }
+    if N & 1 == 0 {
+        N /= 2
+    } else {
+        N = N * 3 + 1
+    }
+}
+
+print(Ans.joined(separator: " "))
+*/
