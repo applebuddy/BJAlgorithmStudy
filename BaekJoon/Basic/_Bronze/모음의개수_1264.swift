f6a0cde1b422ba4593091ff560c4b5aed9d30e88
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-21 16:09:22 +0900

    모음의개수_1264 swift 문제풀이

---
+++ BaekJoon/Basic/_Bronze/모음의개수_1264.swift
@@ -0,0 +1,16 @@
+
+// MARK: - 모음의 개수 1264
+// MARK: swift 문제풀이
+
+/*
+while true {
+    let S = Array(readLine()!)
+    if S.count == 1 && S[0] == "#" { break }
+    var Ans = 0
+    for i in S.indices {
+        if S[i] == "a" || S[i] == "e" || S[i] == "i" || S[i] == "o" || S[i] == "u"
+        || S[i] == "A" || S[i] == "E" || S[i] == "I" || S[i] == "O" || S[i] == "U"{ Ans += 1 }
+    }
+    print(Ans)
+}
+*/
