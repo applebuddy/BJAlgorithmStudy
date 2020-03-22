673956ffc33d08473ecfafc38506a489c533d590
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-22 23:32:08 +0900

    팩토리얼나누기_15996 Math swift 문제풀이

---
+++ BaekJoon/Math/_Silver/팩토리얼나누기_15996.swift
@@ -0,0 +1,16 @@
+
+// MARK: - 팩토리얼 나누기 15996
+// MARK: math swift 문제풀이
+
+/*
+let arr = readLine()!.split(separator: " ").map { Int($0)! }
+let N = arr[0], A = arr[1]
+var val = 1, Ans = 0
+
+while val <= N {
+    val *= A
+    Ans += N / val
+}
+
+print(Ans)
+*/
