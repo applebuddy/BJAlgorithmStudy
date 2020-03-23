6338e5c69f7f8e4395b02d7fe02a5a0de14fbf44
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-23 17:47:38 +0900

    두수의합_3273 TwoPointer swift 문제풀이

---
+++ BaekJoon/TwoPointer/두수의합_3273.swift
@@ -0,0 +1,24 @@
+
+// MARK: - 두수의합 3273
+// MARK: TwoPointer swift 문제풀이
+
+/*
+let N = Int(readLine()!)!
+let arr = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
+let K = Int(readLine()!)!
+var left = 0, right = N-1
+var Ans = 0
+while left < right {
+    if arr[left] + arr[right] == K {
+        Ans += 1
+        left += 1
+        right -= 1
+    } else if arr[left] + arr[right] > K {
+        right -= 1
+    } else {
+        left += 1
+    }
+}
+
+print(Ans)
+*/
