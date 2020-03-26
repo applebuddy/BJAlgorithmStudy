6fef8fbea47597c0399e69a4f165c7621684c74c
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-27 00:34:48 +0900

    팩토리얼_7489 math swift 문제풀이

---
+++ BaekJoon/Math/_Bronze/팩토리얼_7489.swift
@@ -0,0 +1,19 @@
+
+// MARK: - 팩토리얼 7489
+// MARK: math swift 문제풀이
+
+/*
+let T = Int(readLine()!)!
+for _ in 0..<T {
+  let N = Int(readLine()!)!
+  var val = 1
+  for i in 1...N {
+    val *= i
+    while val % 10 == 0 {
+      val /= 10
+    }
+    val %= 100000000
+  }
+  print(val % 10)
+}
+*/
