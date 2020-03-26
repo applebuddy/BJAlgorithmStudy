eb7f1b0a5bbf041c5d5a1aabaeccc930fc4be224
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-27 00:49:09 +0900

    마지막팩토리얼수_2553 math swift 문제풀이

---
+++ BaekJoon/Math/_Gold/마지막팩토리얼수_2553.swift
@@ -0,0 +1,16 @@
+
+// MARK: - 마지막 팩토리얼 수 2553
+// MARK: math swift 문제풀이
+
+/*
+let N = Int(readLine()!)!
+var val = 1
+for i in 1...N {
+  val *= i
+  while val % 10 == 0 {
+    val /= 10
+  }
+  val %= 100000000
+}
+print(val % 10)
+*/
