015f150682d4baa8575c572ddaa3888fb3c05fd3
MinKyeongTae <kyeongtea92@gmail.com>
2020-04-02 19:52:39 +0900

    나는행복합니다~_14652 swift 문제풀이

---
+++ BaekJoon/Math/_Bronze/나는행복합니다_14652.swift
@@ -0,0 +1,9 @@
+
+// MARK: - 나는 행복합니다~ 14652
+// MARK: swift 문제풀이
+
+/*
+let arr = readLine()!.split(separator: " ").map { Int($0)! }
+let N = arr[0], M = arr[1], K = arr[2]
+print(K/M, K%M, separator: " ")
+*/
