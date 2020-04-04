a3d81e4b041fc4931a874d4316491ecffb053774
MinKyeongTae <kyeongtea92@gmail.com>
2020-04-05 00:59:23 +0900

    최대곱_1500 Math swift 문제풀이

--- BaekJoon/Math/_Silver/순서쌍의곱의합_13900.swift
+++ BaekJoon/Math/_Silver/순서쌍의곱의합_13900.swift
@@ -8,12 +8,12 @@ let arr = readLine()!.split(separator: " ").map { Int($0)! }
 
 var DP = [Int](repeating: 0, count: len + 1)
 for i in arr.indices {
-  DP[i+1] = DP[i] + arr[i]
+    DP[i+1] = DP[i] + arr[i]
 }
 
 var Ans = 0
 for i in arr.indices {
-  Ans += arr[i] * (DP[len] - DP[i+1])
+    Ans += arr[i] * (DP[len] - DP[i+1])
 }
 
 print(Ans)
---
+++ BaekJoon/Math/_Silver/최대곱_1500.swift
@@ -0,0 +1,18 @@
+
+// MARK: - 최대곱
+// MARK: Math swift 문제풀이
+
+/*
+let arr = readLine()!.split(separator: " ").map { Int($0)! }
+var S = arr[0], K = arr[1]
+var Ans = 1
+
+while K > 0 {
+  let d = S/K
+  Ans *= d
+  S -= d
+  K -= 1
+}
+
+print(Ans)
+*/
