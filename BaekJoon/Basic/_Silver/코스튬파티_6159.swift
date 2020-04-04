9730563d47e31baa60e5e822263ad97b9afe4532
MinKyeongTae <kyeongtea92@gmail.com>
2020-04-04 21:36:06 +0900

    코스튬파티_6159 swift 문제풀이

---
+++ BaekJoon/Basic/_Silver/코스튬파티_6159.swift
@@ -0,0 +1,27 @@
+
+// MARK: - 코스튬 파티 6159
+// MARK: swift 문제풀이
+
+/*
+let arr = readLine()!.split(separator: " ").map { Int($0)! }
+
+let N = arr[0], S = arr[1]
+var cow = [Int]()
+for _ in 0..<N {
+  cow.append(Int(readLine()!)!)
+}
+
+cow.sort()
+
+var Ans = 0
+
+for i in 0..<N-1 {
+  if cow[i] + cow[i+1] > S { break }
+  for j in i+1..<N {
+    if cow[i] + cow[j] <= S { Ans += 1 }
+    else { break }
+  }
+}
+
+print(Ans)
+*/
