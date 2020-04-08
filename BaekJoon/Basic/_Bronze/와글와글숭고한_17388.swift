6c6b843746f4e799445dc74331f6ce55cecf1dc5
MinKyeongTae <kyeongtea92@gmail.com>
2020-04-08 21:05:16 +0900

    와글와글숭고한_17388 swift 문제풀이

---
+++ BaekJoon/Basic/_Bronze/와글와글숭고한_17388.swift
@@ -0,0 +1,24 @@
+
+// MARK: - 와글와글숭고한 17388
+// MARK: swift 문제풀이
+
+/*
+import Foundation
+
+let arr = readLine()!.split(separator: " ").map { Int($0)! }
+let sum = arr.reduce(0,+)
+var mn = Int(2e9)
+var mdx = 0
+let univ = ["Soongsil", "Korea", "Hanyang"]
+
+if sum >= 100 { print("OK"); exit(0) }
+
+for i in arr.indices {
+  if arr[i] < mn {
+    mn = arr[i]
+    mdx = i
+  }
+}
+
+print(univ[mdx])
+*/
