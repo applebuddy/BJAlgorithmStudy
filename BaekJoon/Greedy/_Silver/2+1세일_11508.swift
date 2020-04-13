2820bc9c7cdeebd2f44578412267b83884746788
MinKyeongTae <kyeongtea92@gmail.com>
2020-04-13 20:04:54 +0900

    2+1세일_11508 Greedy swift 문제풀이

---
+++ BaekJoon/Greedy/_Silver/2+1세일_11508.swift
@@ -0,0 +1,20 @@
+
+// MARK: - 2+1 세일 11508
+// MARK: Greedy swift 문제풀이
+
+/*
+let N = Int(readLine()!)!
+var G = [Int]()
+for _ in 0..<N {
+  G.append(Int(readLine()!)!)
+}
+
+var sum = G.reduce(0,+)
+G.sort(by: >)
+
+for i in G.indices {
+  if (i+1) % 3 == 0 { sum -= G[i] }
+}
+
+print(sum)
+*/
