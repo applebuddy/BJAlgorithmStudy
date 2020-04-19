41813348ae9849451030916ceafc49338c2b5654
MinKyeongTae <kyeongtea92@gmail.com>
2020-04-19 23:50:02 +0900

    이진수_3460 swift 문제풀이

---
+++ BaekJoon/Math/_Bronze/이진수_3460.swift
@@ -0,0 +1,18 @@
+
+// MARK: - 이진수 3460
+// MARK: swift 문제풀이
+
+/*
+let T = Int(readLine()!)!
+for _ in 0..<T {
+  var N = Int(readLine()!)!
+  var idx = 0
+  var Ans = [String]()
+  while N > 0 {
+    if N&1==1 { Ans.append("\(idx)") }
+    N /= 2
+    idx += 1
+  }
+  print(Ans.joined(separator: " "))
+}
+*/
