68ce1f6c41b6d221151c48090376e89845b3855b
MinKyeongTae <kyeongtea92@gmail.com>
2020-04-19 23:33:26 +0900

    피보나치수2_2748 DP swift 문제풀이

---
+++ BaekJoon/DP/_Silver/피보나치수2_2748.swift
@@ -0,0 +1,18 @@
+
+// MARK: - 피보나치수2 2748
+// MARK: DP swift 문제풀이
+
+/*
+let N = Int(readLine()!)!
+var prev = 1, pprev = 1
+if N <= 2 { print(1) }
+else {
+  var Ans = 0
+  for _ in 3...N {
+    Ans = prev + pprev
+    pprev = prev
+    prev = Ans
+  }
+  print(Ans)
+}
+*/
