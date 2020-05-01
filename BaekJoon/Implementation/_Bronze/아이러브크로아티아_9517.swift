2e77ea933797e2030fd96f85a4e28b0f919ce2fe
MinKyeongTae <kyeongtea92@gmail.com>
2020-05-01 12:07:47 +0900

    아이러브크로아티아_9517 Implementation swift 문제풀이

---
+++ BaekJoon/Implementation/_Bronze/아이러브크로아티아_9517.swift
@@ -0,0 +1,24 @@
+
+// MARK: - 아이 러브 크로아티아 9517
+// MARK: swift 문제풀이
+
+/*
+var idx = Int(readLine()!)!
+let N = Int(readLine()!)!
+var time = 0
+
+for _ in 0..<N {
+  let arr = readLine()!.split(separator: " ")
+  let t = Int(String(arr[0]))!
+  let ch = arr[1]
+  time += t
+  if time >= 210 { break }
+
+  if ch == "N" || ch == "P" { continue }
+
+  idx += 1
+  if idx > 8 { idx = 1 }
+}
+
+print(idx)
+*/
