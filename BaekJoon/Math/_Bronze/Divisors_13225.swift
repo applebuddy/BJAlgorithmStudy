e5b9a703ed4681e84dbfccb5453d221e9795efdf
MinKyeongTae <kyeongtea92@gmail.com>
2020-05-01 15:09:54 +0900

    Divisors_13225 math swift 문제풀이

---
+++ BaekJoon/Math/_Bronze/Divisors_13225.swift
@@ -0,0 +1,17 @@
+
+// MARK: - Divisors 13225
+// MARK: math swift 문제풀이
+
+/*
+let N = Int(readLine()!)!
+for _ in 0..<N {
+  let K = Int(readLine()!)!
+  var Ans = 0
+  for i in 1...max(1,K/2) {
+    Ans += (K % i == 0) ? 1 : 0
+  }
+
+  if K != 1 { Ans += 1 }
+  print("\(K) \(Ans)")
+}
+*/
