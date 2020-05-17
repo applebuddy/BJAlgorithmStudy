57c3e84aae88f3ad6c4666739e2d23374c9650d8
MinKyeongTae <kyeongtea92@gmail.com>
2020-05-17 17:59:26 +0900

    더하기사이클_1110 math swift 문제풀이

---
+++ BaekJoon/Math/_Bronze/더하기사이클_1110.swift
@@ -0,0 +1,18 @@
+
+// MARK: - 더하기 사이클 1110
+// MARK: math swift 문제풀이
+
+/*
+var Num = Int(readLine()!)!
+var N = Num
+var Ans = 0
+while true {
+  Ans += 1
+  var now = (N / 10) + (N % 10)
+    now = (N % 10) * 10 + (now % 10)
+  N = now
+  if Num == N { break }
+}
+
+print(Ans)
+*/
