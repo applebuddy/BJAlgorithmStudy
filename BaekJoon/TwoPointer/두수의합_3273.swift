7f5a73ae659eb5c8943800044d769df45017ea44
MinKyeongTae <kyeongtea92@gmail.com>
2020-06-09 19:58:16 +0900

    미세먼지안녕_17144 Implementation swift 복습(최적화) 문제풀이

--- BaekJoon/TwoPointer/두수의합_3273.swift
+++ BaekJoon/TwoPointer/두수의합_3273.swift
@@ -1,24 +1,25 @@
 
 // MARK: - 두수의합 3273
+
 // MARK: TwoPointer swift 문제풀이
 
 /*
-let N = Int(readLine()!)!
-let arr = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
-let K = Int(readLine()!)!
-var left = 0, right = N-1
-var Ans = 0
-while left < right {
-    if arr[left] + arr[right] == K {
-        Ans += 1
-        left += 1
-        right -= 1
-    } else if arr[left] + arr[right] > K {
-        right -= 1
-    } else {
-        left += 1
-    }
-}
+ let N = Int(readLine()!)!
+ let arr = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
+ let K = Int(readLine()!)!
+ var left = 0, right = N-1
+ var Ans = 0
+ while left < right {
+ if arr[left] + arr[right] == K {
+     Ans += 1
+     left += 1
+     right -= 1
+ } else if arr[left] + arr[right] > K {
+     right -= 1
+ } else {
+     left += 1
+ }
+ }
 
-print(Ans)
-*/
+ print(Ans)
+ */
