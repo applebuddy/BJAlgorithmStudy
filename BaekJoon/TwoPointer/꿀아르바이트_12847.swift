e35f0e8b493d1d4d8b08a4037ae557b114636208
MinKyeongTae <kyeongtea92@gmail.com>
2020-06-09 19:40:01 +0900

    꿀아르바이트_12847 twoPointer swift 문제풀이

---
+++ BaekJoon/TwoPointer/꿀아르바이트_12847.swift
@@ -0,0 +1,23 @@
+
+// MARK: - 꿀 아르바이트 12847
+// MARK: TwoPointer swift 문제풀이
+
+/*
+let arr = readLine()!.split(separator: " ").map { Int($0)! }
+let N = arr[0], M = arr[1]
+let G = readLine()!.split(separator: " ").map { Int($0)! }
+
+var sum = G[0..<M].reduce(0, +)
+var Ans = sum
+
+var left = 0, right = M
+while right < G.count {
+  sum -= G[left]
+  sum += G[right]
+  Ans = Ans < sum ? sum : Ans
+  left += 1
+  right += 1
+}
+
+print(Ans)
+*/
