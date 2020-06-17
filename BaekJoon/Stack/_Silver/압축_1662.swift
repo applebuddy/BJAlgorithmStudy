ae484fded28af950e8f87b171db59405248f4421
MinKyeongTae <kyeongtea92@gmail.com>
2020-06-17 22:49:01 +0900

    압축_1662 stack swift 복습(최적화) 문제풀이

--- BaekJoon/Stack/_Silver/압축_1662.swift
+++ BaekJoon/Stack/_Silver/압축_1662.swift
@@ -7,6 +7,31 @@
 //
 
 // MARK: - 압축 1662
+
+// MARK: Stack swift 복습 문제풀이 (최적화)
+/*
+let S = Array(readLine()!)
+var idx = 0
+var G = [[Character]](repeating: [Character](), count: 101)
+var V = [Int](repeating: 0, count: 101)
+
+for i in S.indices {
+  if S[i] == "(" { idx += 1 }
+  else if S[i] == ")" {
+    idx -= 1
+    V[idx] += Int(G[idx].last!.asciiValue! - 48) * V[idx+1] - 1
+    V[idx+1] = 0
+    G[idx+1] = []
+  } else {
+    V[idx] += 1
+    G[idx].append(S[i])
+  }
+}
+
+print(V[0])
+ */
+
+
 // MARK: Stack swift 문제풀이
 
 /*
