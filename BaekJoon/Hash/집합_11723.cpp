c4f640fff6197a90c6cb311b11f195e766c1865b
MinKyeongTae <kyeongtea92@gmail.com>
2020-06-19 20:31:47 +0900

    집합_11723 hash C++ 복습 문제풀이

--- BaekJoon/Hash/집합_11723.cpp
+++ BaekJoon/Hash/집합_11723.cpp
@@ -7,6 +7,41 @@
 //
 
 // MARK: - 집합 11723
+
+// MARK: C++ 복습 문제풀이
+
+#if 0
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios_base :: sync_with_stdio(0); cin.tie(0);
+    vector<bool> UMP(21, false);
+    vector<bool> temp = UMP;
+    for(int i=1; i<=20; i++) temp[i]=true;
+    int T; cin>>T;
+    while(T--) {
+        string S;
+        int N;
+        cin>>S;
+        if(S=="all") {
+            UMP = temp;
+            continue;
+        } else if(S=="empty") {
+            UMP = vector<bool>(21, false);
+            continue;
+        }
+        cin>>N;
+        if(S=="add" && UMP[N]==false) UMP[N]=true;
+        if(S=="remove" && UMP[N]==true) UMP[N]=false;
+        if(S=="check") printf("%d\n",UMP[N] ? 1 : 0);
+        if(S=="toggle") UMP[N] = UMP[N]==false ? true : false;
+    }
+    return 0;
+}
+#endif
+
 // MARK: Hash C++ 문제풀이
 
 #if 0
