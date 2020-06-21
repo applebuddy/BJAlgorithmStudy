4e44d1ae735106647f606bfa3c0c90a38da0f0b5
MinKyeongTae <kyeongtea92@gmail.com>
2020-06-21 11:35:08 +0900

    차집합_1822 Set+Sorting swift 문제풀이

---
+++ BaekJoon/Sorting/_Silver/차집합_1822.swift
@@ -0,0 +1,7 @@
+
+// MARK: - 차집합 1822
+// MARK: Set+Sorting swift 문제풀이
+
+/*
+readLine();var A=Set(readLine()!.split{$0==" "}.map{Int(String($0))!});let B=Set(readLine()!.split{$0==" "}.map{Int(String($0))!});A.subtract(B);if A.count==0{print(0)}else{print(A.count,A.sorted().map{String($0)}.joined(separator:" "),separator: "\n")}
+*/
