a251d25f06b9d301b11ddc439364461533e96884
MinKyeongTae <kyeongtea92@gmail.com>
2020-08-19 03:19:38 +0900

    수정렬하기_2750 swift 숏코딩 문제풀이

---
+++ BaekJoon/Sorting/_Silver/수정렬하기_2750.swift
@@ -0,0 +1,6 @@
+
+// MARK: - 수정렬하기 2750
+
+// MARK: swift 숏코딩 문제풀이
+
+let N = Int(readLine()!)!; var list = [Int](); (0 ..< N).forEach { _ in list.append(Int(readLine()!)!) }; list.sorted().forEach { print($0) }
