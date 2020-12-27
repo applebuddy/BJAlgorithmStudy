154572ea18ec2f32a2691e95db35820a0808029a
MinKyeongTae <kyeongtea92@gmail.com>
2020-12-28 02:51:42 +0900

    음악프로그램_2623 topologySort swift 문제풀이

---
+++ BaekJoon/Sorting/음악프로그램_2623.swift
@@ -0,0 +1,67 @@
+//
+//  음악프로그램_2623.swift
+//  BackJoonPractice
+//
+//  Created by MinKyeongTae on 2020/12/28.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+/*
+import Foundation
+
+let ip: (() -> [Int]) = {
+  let S = readLine()!
+  var tmp = 0
+  var result: [Int] = []
+  S.forEach {
+    if $0 == " " {
+      result.append(tmp)
+      tmp = 0
+      return
+    }
+    tmp = tmp * 10 + Int($0.unicodeScalars.first!.value) - 48
+  }
+  result.append(tmp)
+  return result
+}
+
+let arr = ip()
+let (N, M) = (arr[0], arr[1])
+var G = [[Int]](repeating: [Int](), count: N+1)
+var C = [Int](repeating: 0, count: N+1)
+(0..<M).forEach { _ in
+  let arr2 = ip()
+  let len = arr2[0]
+  for idx in 1..<len {
+    let from = arr2[idx]
+    let to = arr2[idx+1]
+    G[from].append(to)
+    C[to] += 1
+  }
+}
+
+var Q: [Int] = []
+var cur = 0
+(1...N).forEach { node in
+  if C[node] == 0 {
+    Q.append(node)
+  }
+}
+
+var result: [Int] = []
+while cur < Q.count {
+  let node = Q[cur]
+  cur += 1
+  result.append(node)
+  for idx in G[node].indices {
+    let next = G[node][idx]
+    C[next] -= 1
+    if C[next] == 0 {
+      Q.append(next)
+    }
+  }
+}
+
+if result.count != N { print(0); exit(0) }
+print(result.map { "\($0)" }.joined(separator: "\n"))
+*/
