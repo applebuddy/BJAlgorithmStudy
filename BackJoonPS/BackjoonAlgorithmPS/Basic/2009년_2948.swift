1dc380fbd615ff8008907f1c52be7f4d1e775766
MinKyeongTae <kyeongtea92@gmail.com>
2021-01-03 02:27:26 +0900

    2009년_2948 basic swift 문제풀이

---
+++ BackJoonPS/BackjoonAlgorithmPS/Basic/2009년_2948.swift
@@ -0,0 +1,16 @@
+//
+//  2009년_2948.swift
+//  BackjoonAlgorithmPS
+//
+//  Created by MinKyeongTae on 2021/01/03.
+//  Copyright © 2021 MungGu. All rights reserved.
+//
+
+/*
+let arr = readLine()!.split(separator: " ").map { Int(String($0))! }
+let (month, day) = (arr[1], arr[0])
+let mList = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+let dList = ["Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday"]
+print(dList[(mList[0...month-1].reduce(0, +) + day - 1) % 7])
+*/
+
