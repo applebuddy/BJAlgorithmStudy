8170acb0f8457abcc0ae67d44014c9447a51cdfc
MinKyeongTae <kyeongtea92@gmail.com>
2021-09-23 20:01:00 +0900

    백달백준하는명사수_22938 math swift 풀이

---
+++ BackJoonPS/BackjoonAlgorithmPS/Math/백발백준하는명사수_22938.swift
@@ -0,0 +1,21 @@
+//
+//  백발백준하는명사수_22938.swift
+//  BackjoonAlgorithmPS
+//
+//  Created by MinKyeongTae on 2021/09/23.
+//  Copyright © 2021 MungGu. All rights reserved.
+//
+
+/*
+import Foundation
+
+let readInput: (() -> [Double]) = {
+    return readLine()!.split(separator: " ").map { Double(String($0))! }
+}
+
+let ip = readInput()
+let ip2 = readInput()
+let dotDistance = sqrt((ip[0]-ip2[0]) * (ip[0]-ip2[0]) + (ip[1]-ip2[1]) * (ip[1]-ip2[1]))
+let radiusLength = ip[2] + ip2[2]
+print(radiusLength > dotDistance ? "YES" : "NO")
+*/
