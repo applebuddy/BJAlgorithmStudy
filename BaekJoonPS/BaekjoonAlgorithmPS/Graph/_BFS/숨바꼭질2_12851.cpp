2934358527972005e311fd9cd2c17e25df9c0cde
MinKyeongTae <kyeongtea92@gmail.com>
2021-10-24 04:29:44 +0900

    숨바꼭질2_12851, BFS, cpp 풀이

---
+++ BaekJoonPS/BaekjoonAlgorithmPS/Graph/_BFS/숨바꼭질2_12851.cpp
@@ -0,0 +1,71 @@
+//
+//  숨바꼭질2_12851.cpp
+//  BaekjoonAlgorithmPS
+//
+//  Created by MinKyeongTae on 2021/10/24.
+//  Copyright © 2021 MungGu. All rights reserved.
+//
+
+#if 0
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+vector<int> G;
+int N, K;
+int ans;
+vector<int> ansDic(100001, 0);
+
+struct Info {
+    int x;
+    int time;
+    
+    Info(int x, int time) {
+        this->x = x;
+        this->time = time;
+    }
+};
+
+void BFS() {
+    queue<Info> Q;
+    vector<int> chk(100001, 100001);
+    Q.push(Info(N, 0));
+    while(!Q.empty()) {
+        Info node = Q.front();
+        int x = node.x;
+        int time = node.time;
+        Q.pop();
+    
+        if(x==K) {
+            ans = ans > time ? time : ans;
+            ansDic[ans]++;
+            continue;
+        }
+        
+        if(ans >= time+1 && x*2<=100000 && chk[x*2]>=time) {
+            chk[x*2]=time+1;
+            Q.push(Info(x*2, time+1));
+        }
+
+        if(ans >= time+1 && x-1>=0 && chk[x-1]>=time+1) {
+            chk[x-1]=time+1;
+            Q.push(Info(x-1, time+1));
+        }
+        
+        if(ans >= time+1 && x+1<=100000 && chk[x+1]>=time+1) {
+            chk[x+1]=time+1;
+            Q.push(Info(x+1, time+1));
+        }
+    }
+    return;
+}
+
+int main() {
+    ios_base :: sync_with_stdio(); cin.tie(0);
+    cin>>N>>K;
+    ans=abs(K-N);
+    BFS();
+    printf("%d\n%d", ans, ansDic[ans]);
+}
+#endif
